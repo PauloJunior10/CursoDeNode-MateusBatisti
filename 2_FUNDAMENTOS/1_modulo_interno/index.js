c632b923c5b8fe1fb346dc3695120b5f8b72e493
PauloJunior <paulojunior.10@outlook.com>
2023-12-29 20:37:55 -0300

    segundo commit da noite de node

--- 2_FUNDAMENTOS/1_modulo_interno/index.js
+++ 2_FUNDAMENTOS/1_modulo_interno/index.js
@@ -3,3 +3,7 @@ const soma = meuModulo.soma
 
 soma(2,3)
 soma(5,10)
+
+// poderia invocar diretamente sem precisar criar uma variável, porém daria mais trabalho e seria desnecessário, exemplo abaixo
+
+meuModulo.soma(10,10)
